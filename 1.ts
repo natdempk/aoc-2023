4e98608e0dcdbb1c61201ed68f96cc32c9435ef7
Nat Dempkowski <natdempk@gmail.com>
2023-12-01 17:13:38 -0500

    day 1

---
+++ 1.ts
@@ -0,0 +1,45 @@
+export { };
+
+
+const text = await Deno.readTextFile("input1.txt");
+const lines = text.trim().split(/\r?\n/)
+
+
+const digits = new Map<string, number> ([
+  ['one', 1],
+  ['two', 2],
+  ['three', 3],
+  ['four', 4],
+  ['five', 5],
+  ['six', 6],
+  ['seven', 7],
+  ['eight', 8],
+  ['nine', 9]
+]);
+
+function extractNumber(s: string): number {
+  const regexp = /(?=(\d|one|two|three|four|five|six|seven|eight|nine))/g;
+  const matches = Array.from(s.matchAll(regexp), (match) => match[1]);
+  // let matches = s.match(regexp);
+  const firstMatch = matches[0];
+  const lastMatch = matches[matches.length - 1]
+
+  return matchToNum(firstMatch) * 10 + matchToNum(lastMatch)
+}
+
+function matchToNum(s: string): number {
+  if (s.length > 1) {
+    return digits.get(s);
+  } else {
+    return Number(s)
+  }
+}
+
+const numbers = lines.map((l) => {
+  const n = extractNumber(l);
+  // console.log(n + ' ' + l);
+  return n;
+})
+const total = numbers.reduce((sum, x) => sum + x);
+
+console.log(total);
